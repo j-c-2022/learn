70a8b12c6b2de490176154fef5b308157d1e9f9d
j-s-2022 <Coding-2022>
2022-11-05 13:08:18 +0300

    File size calculation (add getFolderSize)

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,25 @@
+import java.io.File;
+import java.util.Set;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
-        System.out.println();
+        String folderPath = "C:\\Users\\Admin\\Desktop\\testFolder";
+        File file = new File(folderPath);
+
+        System.out.println(getFolderSize(file));
+
+
+    }
+
+    public static long getFolderSize(File folder){
+        if(folder.isFile()){
+            return folder.length();
+        }
+        long sum = 0;
+        File [] files = folder.listFiles();
+        for(File file: files){
+            sum +=getFolderSize(file);
+        }
+        return sum;
     }
 }
